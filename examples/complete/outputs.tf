dd0da41acf35357b4a2015372c6877d1a5b1cac5
shanye <shanye.xzq@alibaba-inc.com>
2024-11-25 15:24:21 +0800

    examples: improve the examples

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,3 +1,4 @@
 output "this_alb_listener_id" {
-  value = module.example.this_alb_listener_id
+  value       = module.example.this_alb_listener_id
+  description = "The id of alb listener."
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "this_alb_listener_id" {
-  value = alicloud_alb_listener.default.id
+  value       = alicloud_alb_listener.default.id
+  description = "The id of alb listener."
 }
