cbc0affb3f76392d260b75478e1d7892f961b0c2
shanye <shanye.xzq@alibaba-inc.com>
2024-11-25 15:24:21 +0800

    examples: improve the examples

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -9,7 +9,9 @@ data "alicloud_resource_manager_resource_groups" "default" {
 }
 
 module "vpc" {
-  source             = "alibaba/vpc/alicloud"
+  source  = "alibaba/vpc/alicloud"
+  version = "~>1.11.0"
+
   create             = true
   vpc_cidr           = "172.16.0.0/12"
   vswitch_cidrs      = [cidrsubnet("172.16.0.0/12", 8, 2), cidrsubnet("172.16.0.0/12", 8, 6)]
@@ -17,7 +19,9 @@ module "vpc" {
 }
 
 module "security_group" {
-  source = "alibaba/security-group/alicloud"
+  source  = "alibaba/security-group/alicloud"
+  version = "~>2.4.0"
+
   vpc_id = module.vpc.this_vpc_id
 }
 
@@ -39,13 +43,13 @@ module "example" {
   alb_address_type           = "Internet"
   alb_address_allocated_mode = "Fixed"
   alb_balancer_edition       = var.alb_balancer_edition
-  resource_group_id          = data.alicloud_resource_manager_resource_groups.default.groups.0.id
+  resource_group_id          = data.alicloud_resource_manager_resource_groups.default.groups[0].id
   vpc_id                     = module.vpc.this_vpc_id
   pay_type                   = "PayAsYouGo"
   vswtich_id_1               = module.vpc.this_vswitch_ids[0]
   vswtich_id_2               = module.vpc.this_vswitch_ids[1]
-  zone_id_1                  = data.alicloud_alb_zones.default.zones.0.id
-  zone_id_2                  = data.alicloud_alb_zones.default.zones.1.id
+  zone_id_1                  = data.alicloud_alb_zones.default.zones[0].id
+  zone_id_2                  = data.alicloud_alb_zones.default.zones[1].id
   status                     = var.status
 
   #alicloud_alb_server_group
@@ -55,49 +59,13 @@ module "example" {
   alb_sticky_session_enable      = var.alb_sticky_session_enable
 
   #alicloud_ssl_certificates_service_certificate
-  cert = <<EOF
------BEGIN CERTIFICATE-----
-MIIDRjCCAq+gAwIBAgIJAJn3ox4K13PoMA0GCSqGSIb3DQEBBQUAMHYxCzAJBgNV
-BAYTAkNOMQswCQYDVQQIEwJCSjELMAkGA1UEBxMCQkoxDDAKBgNVBAoTA0FMSTEP
-MA0GA1UECxMGQUxJWVVOMQ0wCwYDVQQDEwR0ZXN0MR8wHQYJKoZIhvcNAQkBFhB0
-ZXN0QGhvdG1haWwuY29tMB4XDTE0MTEyNDA2MDQyNVoXDTI0MTEyMTA2MDQyNVow
-djELMAkGA1UEBhMCQ04xCzAJBgNVBAgTAkJKMQswCQYDVQQHEwJCSjEMMAoGA1UE
-ChMDQUxJMQ8wDQYDVQQLEwZBTElZVU4xDTALBgNVBAMTBHRlc3QxHzAdBgkqhkiG
-9w0BCQEWEHRlc3RAaG90bWFpbC5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJ
-AoGBAM7SS3e9+Nj0HKAsRuIDNSsS3UK6b+62YQb2uuhKrp1HMrOx61WSDR2qkAnB
-coG00Uz38EE+9DLYNUVQBK7aSgLP5M1Ak4wr4GqGyCgjejzzh3DshUzLCCy2rook
-KOyRTlPX+Q5l7rE1fcSNzgepcae5i2sE1XXXzLRIDIvQxcspAgMBAAGjgdswgdgw
-HQYDVR0OBBYEFBdy+OuMsvbkV7R14f0OyoLoh2z4MIGoBgNVHSMEgaAwgZ2AFBdy
-+OuMsvbkV7R14f0OyoLoh2z4oXqkeDB2MQswCQYDVQQGEwJDTjELMAkGA1UECBMC
-QkoxCzAJBgNVBAcTAkJKMQwwCgYDVQQKEwNBTEkxDzANBgNVBAsTBkFMSVlVTjEN
-MAsGA1UEAxMEdGVzdDEfMB0GCSqGSIb3DQEJARYQdGVzdEBob3RtYWlsLmNvbYIJ
-AJn3ox4K13PoMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAY7KOsnyT
-cQzfhiiG7ASjiPakw5wXoycHt5GCvLG5htp2TKVzgv9QTliA3gtfv6oV4zRZx7X1
-Ofi6hVgErtHaXJheuPVeW6eAW8mHBoEfvDAfU3y9waYrtUevSl07643bzKL6v+Qd
-DUBTxOAvSYfXTtI90EAxEG/bJJyOm5LqoiA=
------END CERTIFICATE-----
-EOF
-  key  = <<EOF
------BEGIN RSA PRIVATE KEY-----
-MIICXAIBAAKBgQDO0kt3vfjY9BygLEbiAzUrEt1Cum/utmEG9rroSq6dRzKzsetV
-kg0dqpAJwXKBtNFM9/BBPvQy2DVFUASu2koCz+TNQJOMK+BqhsgoI3o884dw7IVM
-ywgstq6KJCjskU5T1/kOZe6xNX3Ejc4HqXGnuYtrBNV118y0SAyL0MXLKQIDAQAB
-AoGAfe3NxbsGKhN42o4bGsKZPQDfeCHMxayGp5bTd10BtQIE/ST4BcJH+ihAS7Bd
-6FwQlKzivNd4GP1MckemklCXfsVckdL94e8ZbJl23GdWul3v8V+KndJHqv5zVJmP
-hwWoKimwIBTb2s0ctVryr2f18N4hhyFw1yGp0VxclGHkjgECQQD9CvllsnOwHpP4
-MdrDHbdb29QrobKyKW8pPcDd+sth+kP6Y8MnCVuAKXCKj5FeIsgVtfluPOsZjPzz
-71QQWS1dAkEA0T0KXO8gaBQwJhIoo/w6hy5JGZnrNSpOPp5xvJuMAafs2eyvmhJm
-Ev9SN/Pf2VYa1z6FEnBaLOVD6hf6YQIsPQJAX/CZPoW6dzwgvimo1/GcY6eleiWE
-qygqjWhsh71e/3bz7yuEAnj5yE3t7Zshcp+dXR3xxGo0eSuLfLFxHgGxwQJAAxf8
-9DzQ5NkPkTCJi0sqbl8/03IUKTgT6hcbpWdDXa7m8J3wRr3o5nUB+TPQ5nzAbthM
-zWX931YQeACcwhxvHQJBAN5mTzzJD4w4Ma6YTaNHyXakdYfyAWrOkPIWZxfhMfXe
-DrlNdiysTI4Dd1dLeErVpjsckAaOW/JDG5PCSwkaMxk=
------END RSA PRIVATE KEY-----
-EOF
+  cert = var.ssl_cert
+  key  = var.ssl_key
+
   #alicloud_alb_listener
   alb_listener_protocol    = "HTTP"
   port                     = 8886
   alb_listener_description = var.alb_listener_description
-  region                   = data.alicloud_regions.default.regions.0.id
+  region                   = data.alicloud_regions.default.regions[0].id
 
 }
